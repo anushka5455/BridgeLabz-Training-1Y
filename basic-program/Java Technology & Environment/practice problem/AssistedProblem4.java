bcb23723fe47741edd6c648f2ed71804e01e75a8
anushka5455 <anushkaupadhyay25@gmail.com>
2026-02-14 10:12:57 +0530

    practice problems

---
+++ basic-program/Java Technology & Environment/practice problem/AssistedProblem4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AssistedProblem4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius of the circle: ");
+        double radius = sc.nextDouble();
+
+        double area = Math.PI * radius * radius;
+
+        System.out.println("Area of the circle = " + area);
+
+        sc.close();
+    }
+}
